80eaae1d895733ebeaf7d434245af3c72f5ee2c3
EricApgar <ericapgar1@gmail.com>
2022-12-13 21:51:04 -0600

    Readme updates with new idea.

--- README.md
+++ README.md
@@ -19,3 +19,16 @@ conda install ffmpeg
 ```
 
 You should see this as part of the virtual environment (SpeechToText).
+
+# Problem Notes:
+* There's always a problem of cutting off a speaker mid-word if you sample audio at a set interval.
+* You usually sample audio for x seconds (I've been doing x=1) and then pipe that sound to the model.
+    * The problem is that the model takes about 1/5 of a second to run - longer for a longer segment. This delay isn't that long but by the time you predict and go back to sampling audio, you've lost a decent amount of time which could be in the middle of a word and that throws off all the predictions.
+* If you wait to send audio to the model before you 
+* Even if you don't sample at a set interval and you wait to detect a sound befor your ,
+
+## Idea:
+* Record audio for a very short amount of time (.2 seconds?) and immediately analyze the numpy array to see if the audio level has dropped to zero (or just a low number). This signals the end of a word. Make sure that the pause is long enough that its not just a pause in the middle of the word. If it's not the end, then keep reading audio samples into the array until you hit the dead zone.
+    * Checking if the end of the array is a dead zone should be such a quick operation that the time away from recording audio should hopefully not cut out enough audio to mess with the sample. Have to check this though.
+* Once the dead zone is confirmed, send the sample to the model.
+* Repeat the process.
--- model_streaming.py
+++ model_streaming.py
@@ -38,6 +38,7 @@ def record_sample(length: float=1) -> None:
     CHANNELS = 1  #2
     RATE = 16000  #44100
 
+    start_time = time.time()  # REMOVE.
     p = pyaudio.PyAudio()
 
     stream = p.open(
@@ -46,16 +47,25 @@ def record_sample(length: float=1) -> None:
         rate=RATE,
         input=True,
         frames_per_buffer=CHUNK)
+    elapsed_time = time.time() - start_time  # REMOVE.
+    print(f'Time to open stream: {elapsed_time:2f}')  # REMOVE.
 
     numpy_data = []
 
-    for _ in range(0, int(RATE / CHUNK * length)):
+    start_time = time.time()  # REMOVE.
+    while (time.time() - start_time) < length:
+    # for _ in range(0, int(RATE / CHUNK * length)):
         data = stream.read(CHUNK)
         numpy_data.extend(np.frombuffer(data, dtype=np.int16))
+    elapsed_time = time.time() - start_time  # REMOVE.
+    print(f'Time to build sound array: {elapsed_time:2f}')  # REMOVE.
 
+    start_time = time.time()  # REMOVE.
     stream.stop_stream()
     stream.close()
     p.terminate()
+    elapsed_time = time.time() - start_time  # REMOVE.
+    print(f'Time to cleanup stream: {elapsed_time:2f}')  # REMOVE.
 
     return np.double(numpy_data)
 
@@ -81,13 +91,23 @@ def main(stop_time: float=10) -> None:
 
     start_time = time.time()
 
+    full_transcription = []
+
     while (time.time() - start_time) < stop_time:
 
+        print_time = time.time()
         data = record_sample(length=2)
+        elapsed_time = time.time() - print_time
+        print(f'Time to record: {elapsed_time:2f}')
 
+        print_time = time.time()
         transcription = transcribe_audio(data, model=model, processor=processor)
+        elapsed_time = time.time() - print_time
+        print(f'Time to Transcribe: {elapsed_time:2f}')
 
-        print(transcription)
+        full_transcription.extend(transcription)
+
+    print(' '.join(full_transcription))
 
     return
 
