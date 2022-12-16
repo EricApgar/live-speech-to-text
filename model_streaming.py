9eaeebb0cac063aca31d307df2ad96876c2cebde
EricApgar <ericapgar1@gmail.com>
2022-12-15 18:57:20 -0600

    Cleanup code.

--- model_streaming.py
+++ model_streaming.py
@@ -5,10 +5,6 @@ import pyaudio
 import time
 import numpy as np
 
-# import matplotlib.pyplot as plt
-# plt.plot([data])
-# plot.show()
-
 
 def working_test():
 
@@ -34,80 +30,6 @@ def working_test():
     print(transcription)
 
     return
-
-def record_sample(length: float=1) -> None:
-
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1  #2
-    RATE = 16000  #44100
-
-    # start_time = time.time()  # REMOVE.
-    p = pyaudio.PyAudio()
-
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK)
-    # elapsed_time = time.time() - start_time  # REMOVE.
-    # print(f'Time to open stream: {elapsed_time:2f}')  # REMOVE.
-
-    ready_for_model = False
-    full_sample = []
-
-    start_time = time.time()  # REMOVE.
-    while (time.time() - start_time) < length:
-        
-        sample = read_audio_stream(stream=stream, chunk_size=CHUNK, read_time_s=.1)
-        
-        if activity_has_stopped(sample=sample):
-
-            full_sample.extend(sample)
-            ready_for_model = True
-
-        elif ready_for_model:  # Means dead zone found after a sample with activity.
-
-            break
-
-
-    numpy_data = []
-
-    start_time = time.time()  # REMOVE.
-    while (time.time() - start_time) < length:
-    # for _ in range(0, int(RATE / CHUNK * length)):
-        chunk_start = time.time()
-        
-        data = stream.read(CHUNK)  # Time to read CHUNK is approx (1/SAMPLE_RATE * CHUNK) seconds.
-        audio_sample = np.double(np.frombuffer(data, dtype=np.int16))  # Numerical audio sample.
-        numpy_data.extend(audio_sample)
-
-        print(f'{time.time() - chunk_start:.3f} seconds to read chunk.')
-
-        # Determine if the data is at the end of a word.
-        threshold_10_percent = .1 * max(audio_sample)
-        num_below = sum(abs(i) < threshold_10_percent for i in audio_sample[-10:])
-        if num_below > 5:
-            break
-
-    # Continue to record samples and throw them away unless it looks like there's activity happening.
-    # (Same as above statement) Dont pass on a sample to the model unless it looks like there's activity in it.
-    # Once there is a sample that has activity, continue to add to it until there is a lull in activity.
-    # Then stop recording and send that sample to the model.
-    # Difficulties lie in fast detection of activity.
-
-    # elapsed_time = time.time() - start_time  # REMOVE.
-    # print(f'Time to build sound array: {elapsed_time:2f}')  # REMOVE.
-
-    # start_time = time.time()  # REMOVE.
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    # elapsed_time = time.time() - start_time  # REMOVE.
-    # print(f'Time to cleanup stream: {elapsed_time:2f}')  # REMOVE.
-
-    return numpy_data  #np.double(numpy_data)
 
 def detect_and_sample() -> np.array:
 
@@ -167,8 +89,6 @@ def activity_levels(sample: np.array):
     '''
     Determine if a sample has any activity, and if it ends in a dead zone. 
     This means that it's the end of a word. 
-    TODO: what happens if the dead zone is in the middle of the array? Hopefully
-    not an issue. Looking for a longish pause signifying the end of a single word.
     '''
 
     SOUND_THRESHOLD = 1000
@@ -193,49 +113,37 @@ def activity_levels(sample: np.array):
 
     return is_active, ends_dead
 
-def transcribe_audio(audio_array, model, processor):
+def transcribe_audio(audio_array, model, processor) -> list:
 
-    # tokenize
-    input_values = processor(audio_array, return_tensors="pt", padding="longest").input_values  # Batch size 1
+    # Tokenize.
+    input_values = processor(audio_array, return_tensors="pt", padding="longest").input_values  # Batch size 1.
 
-    # retrieve logits
+    # Retrieve logits.
     logits = model(input_values).logits
 
-    # take argmax and decode
+    # Take argmax and decode.
     predicted_ids = torch.argmax(logits, dim=-1)
     transcription = processor.batch_decode(predicted_ids)
 
     return transcription
 
-def main(stop_time: float=10) -> None:
+def main(run_time: float=10) -> None:
 
-    # load model and tokenizer
+    # Load model and tokenizer.
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
     model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
     start_time = time.time()
 
-    # full_transcription = []
+    while (time.time() - start_time) < run_time:
 
-    while (time.time() - start_time) < stop_time:
-
-        # print_time = time.time()
-        # data = record_sample(length=2)
-        # elapsed_time = time.time() - print_time
-        # print(f'Time to record: {elapsed_time:2f}')
+        print('Waiting for sound...')
 
         data = detect_and_sample()
 
-        # print_time = time.time()
         transcription = transcribe_audio(data, model=model, processor=processor)
-        # elapsed_time = time.time() - print_time
-        # print(f'Time to Transcribe: {elapsed_time:2f}')
-
-        # full_transcription.extend(transcription)
 
         print(transcription[0])
-
-    # print(' '.join(full_transcription))
 
     return
 
@@ -243,5 +151,4 @@ def main(stop_time: float=10) -> None:
 # MAIN:
 main()
 
-# working_test()
 
