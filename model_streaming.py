00847730c8489059fcf1e67c760dc17771bbe445
EricApgar <ericapgar1@gmail.com>
2023-01-10 19:25:23 -0600

    Added requirements and debugging flag.

--- model_streaming.py
+++ model_streaming.py
@@ -5,9 +5,11 @@ import pyaudio
 import time
 import numpy as np
 
-# import wave
-# import pathlib
-# import os
+# For Debugging.
+from scipy.io.wavfile import write as wav_write
+import pathlib
+import os
+import matplotlib.pyplot as plt
 
 
 def working_test():
@@ -35,14 +37,13 @@ def working_test():
 
     return
 
-def detect_and_sample() -> np.array:
+def detect_and_sample(show_plot: bool=False) -> np.array:
 
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
     CHANNELS = 1  #2
     RATE = 16000  #44100
 
-    # start_time = time.time()  # REMOVE.
     p = pyaudio.PyAudio()
 
     stream = p.open(
@@ -55,14 +56,14 @@ def detect_and_sample() -> np.array:
     ready_for_model = False  # Initialize to no good sample found.
     full_sample = []
 
-    while True:
+    while True:  # Break out after creating a good sample.
         
         sample = read_audio_stream(stream=stream, chunk_size=CHUNK, read_time_s=.2)
 
         is_active, ends_dead = activity_levels(sample=sample)
 
-        if is_active:
-            full_sample.extend(sample)
+        if is_active:  # Has something that looks like non-background sound.
+            full_sample.extend(sample)  # Add snippet to the full sample array.
             ready_for_model = True
 
         if ready_for_model and ends_dead:  # Means dead zone found after a sample with activity.
@@ -73,12 +74,16 @@ def detect_and_sample() -> np.array:
     stream.close()
     p.terminate()
 
-    # write_audio(
-    #     p=p, 
-    #     frames=full_raw, 
-    #     n_channels=CHANNELS, 
-    #     format=FORMAT, 
-    #     sample_rate=RATE)
+    if show_plot:  # DEBUGGING:
+
+        # Write sample to wav.
+        repo_folder = os.path.dirname(str(pathlib.Path(__file__).absolute()))
+        audio_file = os.path.join(repo_folder, 'Temp Audio Files', 'audio.wav')
+        wav_write(audio_file, RATE, np.array(full_sample).astype(np.int16))
+        
+        # Plotting sample.
+        plt.plot(full_sample)
+        plt.show()
 
     return full_sample
 
@@ -98,27 +103,29 @@ def read_audio_stream(stream, read_time_s: float=.1, chunk_size: int=1024) -> np
 
 def activity_levels(sample: np.array):
     '''
-    Determine if a sample has any activity, and if it ends in a dead zone. 
-    This means that it's the end of a word. 
+    Determine if an audio sample has any activity, and if it ends in a dead zone. 
+    If a sample has activity
     '''
 
-    SOUND_THRESHOLD = 1000
-    PERCENT_ACTIVE_REQUIRED = 10
-    PERCENT_LOOK_BACK = 25
+    SOUND_THRESHOLD = 1000  # Anything below this is considered background noise. Above is considered "active".
+    PERCENT_ACTIVE_REQUIRED = 10  # This percent samples over the SOUND_THRESHOLD is considered an "active" clip. 
+    PERCENT_LOOK_BACK = 25  # If there no above-noise-threshold elements in this "back percent" of the sample, its the end of the clip.
 
-    is_active = False  # Initial values.
-    ends_dead = True  # Initial values.
+    is_active = False  # Initial state for sample has active points.
+    ends_dead = True  # Initial for the clip ending in a non-active state.
 
     sample_length = len(sample)
 
-    n_look_back = int(np.round(PERCENT_LOOK_BACK/100 * sample_length))
-    num_active_required = int(np.round(PERCENT_ACTIVE_REQUIRED/100 * sample_length))
+    n_look_back = int(np.round(PERCENT_LOOK_BACK/100 * sample_length))  # Number of frames to look back on.
+    num_active_required = int(np.round(PERCENT_ACTIVE_REQUIRED/100 * sample_length))  # Num active frames required.
 
+    # This is a list of the all the indexes of the frames that were considered "active".
     i_active = [i for i, value in enumerate(sample) if abs(value) > SOUND_THRESHOLD]
 
-    if len(i_active) >= num_active_required:
+    if len(i_active) >= num_active_required:  # Total number of active frames is above threshold...
         is_active = True
     
+        # If the last active frame is later than the percent look back frame...
         if i_active[-1] > (sample_length - n_look_back):  # Last active value.
             ends_dead = False
 
@@ -129,7 +136,7 @@ def transcribe_audio(audio_array, model, processor) -> list:
     SAMPLING_RATE = 16000 # Standard, but should be same as in detect_and_sample().
 
     # Tokenize.
-    input_values = processor(audio_array, return_tensors="pt", padding="longest", sampling_rate=16000).input_values  # Batch size 1.
+    input_values = processor(audio_array, return_tensors="pt", padding="longest", sampling_rate=SAMPLING_RATE).input_values  # Batch size 1.
 
     # Retrieve logits.
     logits = model(input_values).logits
@@ -140,7 +147,7 @@ def transcribe_audio(audio_array, model, processor) -> list:
 
     return transcription
 
-def main(run_time: float=10) -> None:
+def main(run_time: float=10, show_plot: bool=False) -> None:
 
     # Load model and tokenizer.
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
@@ -152,7 +159,7 @@ def main(run_time: float=10) -> None:
 
         print('Waiting for sound...')
 
-        data = detect_and_sample()
+        data = detect_and_sample(show_plot=show_plot)
 
         transcription = transcribe_audio(data, model=model, processor=processor)
 
@@ -160,22 +167,8 @@ def main(run_time: float=10) -> None:
 
     return
 
-def write_audio(p, frames: list, n_channels, format, sample_rate) -> None:
-
-    repo_folder = os.path.dirname(str(pathlib.Path(__file__).absolute()))
-    audio_file = os.path.join(repo_folder, 'Temp Audio Files', 'audio.wav')
-
-    wf = wave.open(audio_file, 'wb')
-    wf.setnchannels(n_channels)
-    wf.setsampwidth(p.get_sample_size(format))
-    wf.setframerate(sample_rate)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    return
-
 
 # MAIN:
-main(run_time=100)
+main(run_time=100, show_plot=False)
 
 
