8096a7670881f22a4bd085c6b81184dcce211782
EricApgar <ericapgar1@gmail.com>
2024-02-03 21:24:04 -0600

    Update nice prints.

--- continuous_asr.py
+++ continuous_asr.py
@@ -10,13 +10,15 @@ print('Setting silence threshold...')
 audio.set_silence_threshold()
 print('Done.\n')
 
+print('Waiting to transcribe...\n')
 while True:
     
     audio.record_activity()
     
     text = model.transcribe_audio_array(audio_array=audio.data, sample_rate_hz=audio.rate_hz)
 
-    print(text[0])
+    if text[0] != 'you':  # Fix for whisper model's default response for random noise.
+        print(text[0])
 
     if 'stop' in text[0].lower():
         break
