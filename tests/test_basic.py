7ca94d4e59f60d4c69dd24d0c45454b02a08d5ea
EricApgar <ericapgar1@gmail.com>
2025-01-25 18:06:46 -0600

    Adding testing file.

---
+++ tests/test_basic.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+repo_dir = os.path.dirname(script_dir)
+sys.path.append(repo_dir)
+
+from audio_object import Audio
+from AsrModels.openAiWhisper import OpenAiWhisperModel
+from continuous_asr import get_recording_sample_rate
+
+
+model = OpenAiWhisperModel()
+model_rate_hz = model.get_sample_rate()
+print(f'Model: OpenAI Whisper, Sample Rate (Hz): {model_rate_hz}')
+
+# Get the recording device sample rate closest to the model's sample rate.
+record_rate_hz = get_recording_sample_rate(input_device_index=11, target_rate_hz=model_rate_hz)
+print(f'Recording Rate (Hz): {record_rate_hz}')
+
+A = Audio(input_device_index=11)
+
+frames_per_buffer_test = [256, 512, 1024]
+
+for frames_per_buffer in frames_per_buffer_test:
+    
+    try:
+        A._open_stream(rate_hz=record_rate_hz, frames_per_buffer=frames_per_buffer)
+        data = A._read_stream()
+        A._close_stream()
+
+        print(f'Valid frames_per_buffer: {frames_per_buffer}')
+    except:
+        pass
